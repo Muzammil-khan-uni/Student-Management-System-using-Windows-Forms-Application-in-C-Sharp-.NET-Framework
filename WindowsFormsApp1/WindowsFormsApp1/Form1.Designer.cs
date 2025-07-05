7ea3d9acaeff063a75d4c0a46130d9a3a4258258
Muzammil Khan <muz56565@gmail.com>
2025-07-05 16:53:26 +0500

    Student Management System – Windows Forms Application

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSubmit = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnNext = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.rollno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnload = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnSubmit
+            // 
+            this.BtnSubmit.Location = new System.Drawing.Point(157, 232);
+            this.BtnSubmit.Name = "BtnSubmit";
+            this.BtnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.BtnSubmit.TabIndex = 0;
+            this.BtnSubmit.Text = "Submit";
+            this.BtnSubmit.UseVisualStyleBackColor = true;
+            this.BtnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(317, 232);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 23);
+            this.BtnClear.TabIndex = 1;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnNext
+            // 
+            this.BtnNext.Location = new System.Drawing.Point(504, 232);
+            this.BtnNext.Name = "BtnNext";
+            this.BtnNext.Size = new System.Drawing.Size(75, 23);
+            this.BtnNext.TabIndex = 2;
+            this.BtnNext.Text = "Next";
+            this.BtnNext.UseVisualStyleBackColor = true;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(276, 106);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(303, 20);
+            this.name.TabIndex = 3;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(276, 177);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(303, 20);
+            this.email.TabIndex = 4;
+            // 
+            // rollno
+            // 
+            this.rollno.Location = new System.Drawing.Point(276, 144);
+            this.rollno.Name = "rollno";
+            this.rollno.Size = new System.Drawing.Size(303, 20);
+            this.rollno.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Roll No.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Email";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(157, 279);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(422, 153);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // btnload
+            // 
+            this.btnload.Location = new System.Drawing.Point(329, 449);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(75, 23);
+            this.btnload.TabIndex = 10;
+            this.btnload.Text = "Load";
+            this.btnload.UseVisualStyleBackColor = true;
+            this.btnload.Click += new System.EventHandler(this.btnload_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Location = new System.Drawing.Point(157, 449);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(75, 23);
+            this.btn_Update.TabIndex = 11;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(503, 448);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 12;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(819, 484);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btnload);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rollno);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.BtnNext);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnSubmit);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSubmit;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnNext;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox rollno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            string Name = name.Text;
+            string RollNo = rollno.Text;
+            string Email = email.Text;
+            string connectionString = ConfigurationManager.ConnectionStrings["Lab2"].ConnectionString;
+
+            string query = "INSERT INTO STUDENTDB (Name,Roll_No,Email) VALUES (@Name,@Roll_No,@Email)";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Name", Name);
+                    cmd.Parameters.AddWithValue("@Roll_No", RollNo);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+
+                    int Result = cmd.ExecuteNonQuery();
+
+                    if (Result > 0)
+                    {
+                        MessageBox.Show("Record inserted Sucessfully");
+                    }
+                    else
+                    {
+                        MessageBox.Show("There some error");
+                    }
+
+                }
+
+
+            }
+
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            name.Text = "";
+            rollno.Text = "";
+            email.Text = "";
+        }
+
+        private void btnload_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Lab2"].ConnectionString;
+            string query = "SELECT * FROM STUDENTDB";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            string Name = name.Text;
+            string RollNo = rollno.Text;
+            string Email = email.Text;
+            string connectionString = ConfigurationManager.ConnectionStrings["Lab2"].ConnectionString;
+
+            string query = "UPDATE STUDENTDB SET Name = @Name, Email = @Email WHERE Roll_No = @Roll_No";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Name", Name);
+                    cmd.Parameters.AddWithValue("@Roll_No", RollNo);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+
+                    int Result = cmd.ExecuteNonQuery();
+                    if (Result > 0)
+                    {
+                        MessageBox.Show("Record updated successfully");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No record found with the given Roll No");
+                    }
+                }
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            string RollNo = rollno.Text;
+    string connectionString = ConfigurationManager.ConnectionStrings["Lab2"].ConnectionString;
+
+    string query = "DELETE FROM STUDENTDB WHERE Roll_No = @Roll_No";
+    using (SqlConnection conn = new SqlConnection(connectionString))
+    {
+        conn.Open();
+        using (SqlCommand cmd = new SqlCommand(query, conn))
+        {
+            cmd.Parameters.AddWithValue("@Roll_No", RollNo);
+
+            int Result = cmd.ExecuteNonQuery();
+            if (Result > 0)
+            {
+                MessageBox.Show("Record deleted successfully");
+            }
+            else
+            {
+                MessageBox.Show("No record found with the given Roll No");
+            }
+        }
+    }
+        }
+    }
+}
